b4f65647dba2b41e5237ef59b7d69ae6d2d66b6b
tqyCom <123234723@qq.com>
2018-03-05 16:04:27 +0800

    zhuoqiu

---
+++ billiards/game.js
@@ -0,0 +1,5 @@
+require('libs/weapp-adapter-min');
+var Parser = require('libs/xmldom/dom-parser');
+window.DOMParser = Parser.DOMParser;
+require('src/settings.fcc57');
+require('main.b8240');
---
+++ billiards/src/settings.fcc57.js
@@ -0,0 +1,127 @@
+window._CCSettings = {
+    platform: "wechatgame",
+    groupList: [
+        "default",
+        "edge",
+        "pocket",
+        "cue",
+        "ball",
+        "white_ball"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false,
+            null,
+            null,
+            true,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            null,
+            true,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            null,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            false,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            true,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "0bgAzZQ79GXYNN/ka9lsLK": [
+                "res/bbbj.png",
+                0
+            ],
+            "3452MtCVFICK3nOQTbzVo1": [
+                "res/qiuzhuo.png",
+                0
+            ],
+            "9f+p3njqBOkInOgRPL0zsp": [
+                "res/sprite_sheet.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/scenes/game_scene.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/game_scene.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "065b1b4a1": [
+            0,
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "35Yg5EorFASLbqCYAob7vl",
+            "3ffTMgmIJDHqCmzZW3RcSF",
+            "5a97XTwzpGWqayKbp8gn8Y",
+            "63n88rgz5Pc74nEJ5zkbzw",
+            "6eRZ/8i5RCXLWw4NT5lhYA",
+            "6eT+dJu9pI1ZcJCgvAF8E1",
+            "7cYWZ8TUBPGph34h27e0Ki",
+            "9bs7abB2FAjYVKL+LCLZ8T",
+            "b0SCcqnNlMMJRHLYVURu7P",
+            "e0AKMoqBpHdZ0znX5pMu2e",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "2377eA2exLhqCJfE7/0htb"
+    ],
+    "md5AssetsMap": {
+        "06/065b1b4a1.json": "cfa20",
+        "assets/res/bbbj.png": "b6e58",
+        "assets/res/qiuzhuo.png": "e3d04",
+        "assets/res/sprite_sheet.png": "d8bee",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4"
+    }
+}
